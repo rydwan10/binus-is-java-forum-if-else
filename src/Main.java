cb3c40424abed637155bd18c385811b8c5683ee8
Muhammad Rydwan <rydwan.dev@gmail.com>
2022-09-20 19:01:41 +0700

    Refactor

--- src/Main.java
+++ src/Main.java
@@ -7,38 +7,23 @@ class Cart {
     private String itemName;
     private int quantity;
     private double price;
-
     public String getItemName() {
         return itemName;
     }
-
     public void setItemName(String itemName) {
         this.itemName = itemName;
     }
-
     public int getQuantity() {
         return quantity;
     }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
     public double getPrice() {
         return price;
     }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
     public Cart(String itemName, int quantity, double price) {
         this.itemName = itemName;
         this.quantity = quantity;
         this.price = price;
     }
-
-
 }
 
 public class Main {
@@ -75,6 +60,5 @@ public class Main {
             }
         }
     }
-
 }
 
